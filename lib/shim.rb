e8e97ba666c6cec222352dbadf15ca7b18ccafda
Corey Woodcox <corey.woodcox@gmail.com>
2012-06-12 11:27:45 -0600

    class << self doesn't work like i thought it should

--- lib/shim.rb
+++ lib/shim.rb
@@ -1,17 +1,15 @@
 require "shim/version"
 
 module Shim
-  class << self
-    attr_accessor :layout, :content_path
-    @layout = "application"
-    @content_path = "shim/"
+  mattr_accessor :layout, :content_path
+  @@content_path = "shim/"
+  @@layout = "application"
 
-    def config(&block)
-      if block.arity == 1
-        self[&block]
-      else
-        instance_eval &block
-      end
+  def config(&block)
+    if block.arity == 1
+      self[&block]
+    else
+      instance_eval &block
     end
   end
 
